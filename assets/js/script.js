725bd16e18cf4867e7458d4e93d90a74e9568625
Sanchay Singh <sanchayofficial@gmail.com>
2025-12-24 17:24:00 +0530

    Add smooth scrolling functionality for anchor links

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.scroll-link').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href');
+    const target = document.querySelector(targetId);
+
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
